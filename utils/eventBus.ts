dddbb6b0d01d82d085d48972b54f7d47438a336d
超级无敌数码暴龙战士 <67992313+konodioda727@users.noreply.github.com>
2024-09-16 14:24:20 +0800

    feat: 新增eventBus

---
+++ utils/eventBus.ts
@@ -0,0 +1,32 @@
+export class EventBus {
+  _eventPool: Record<string, (() => void)[]> = {};
+  emit(name: string) {
+    if (this._eventPool[name] instanceof Array) {
+      this._eventPool[name].forEach(fn => fn());
+      return;
+    }
+    throw new Error(`没有名为 ${name} 的事件`);
+  }
+  on(name: string, fn: () => void) {
+    const eventFns = this._eventPool[name];
+    // eslint-disable-next-line no-unused-expressions
+    eventFns ? eventFns.push(fn) : (this._eventPool[name] = [fn]);
+  }
+  off(name: string, fn: () => void) {
+    const eventFns = this._eventPool[name];
+    if (!eventFns) {
+      throw new Error(`${name} 中不存在该函数`);
+    }
+    this._eventPool[name].splice(this._eventPool[name].indexOf(fn), 1);
+  }
+  once(name: string, fn: () => void) {
+    const onceFn = () => {
+      fn();
+      this.off(name, fn);
+    };
+    this.on(name, onceFn);
+  }
+}
+
+const globalEventBus = new EventBus();
+export default globalEventBus;
