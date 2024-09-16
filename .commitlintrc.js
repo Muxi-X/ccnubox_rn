e27d4639791c09068df49b232f0847cc55bd650b
超级无敌数码暴龙战士 <67992313+konodioda727@users.noreply.github.com>
2024-09-16 14:35:16 +0800

    feat: 新增提交时prettier格式化

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [2, 'always', [
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
         'feat', // 新功能（feature）
         'fix', // 修补bug
         'docs', // 文档（documentation）
@@ -12,10 +15,10 @@ module.exports = {
         'chore', // 构建过程或辅助工具的变动
         'revert', // 回滚到上一个版本
         'build', // 影响构建系统或外部依赖的更改
-        'ci' // 更改CI配置文件和脚本
-      ]],
-      'subject-case': [2, 'never', ['start-case', 'pascal-case']], // 提交信息主题部分不能使用大写开头或Pascal Case
-      'header-max-length': [2, 'always', 72] // 提交信息的最大长度
-    }
-  };
-  
+        'ci', // 更改CI配置文件和脚本
+      ],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case']], // 提交信息主题部分不能使用大写开头或Pascal Case
+    'header-max-length': [2, 'always', 72], // 提交信息的最大长度
+  },
+};
