5c95c27d177063ddc9f6547c73843edeabf32139
lilith xia <xiazhizhizhi@gmail.com>
2024-11-16 15:09:24 +0800

    feat: 课表静态

--- constants/tabBar.tsx
+++ constants/tabBar.tsx
@@ -34,6 +34,15 @@ export const tabConfig: SingleTabType[] = [
     name: 'schedule',
     title: '日程',
     iconName: 'calendar',
+    headerTitle: () => (
+      <Text style={[commonStyles.TabBarPadding, commonStyles.fontLarge]}> 日程 </Text>
+    ),
+    headerRight: () => (
+      <MaterialIcons
+        name="menu"
+        style={commonStyles.TabBarPadding}
+        ></MaterialIcons>
+    ),
   },
   {
     name: 'notification',
