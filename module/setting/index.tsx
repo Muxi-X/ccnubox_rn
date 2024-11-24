81047d673a09954acb980b4263621f60c38a470e
eleliauk <2831336720@qq.com>
2024-11-24 19:02:08 +0800

    feat(setting): 个性化按钮迁移

--- module/setting/index.tsx
+++ module/setting/index.tsx
@@ -1,49 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import * as Updates from 'expo-updates';
-import { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
 
-import Button from '@/components/button';
-import Modal from '@/components/modal';
-import Picker from '@/components/picker';
-import Text from '@/components/text';
-import Toast from '@/components/toast';
 import ThemeBasedView from '@/components/view';
 
-import useVisualScheme from '@/store/visualScheme';
-
-import SettingItem from '@/module/setting/components/settingItem';
 import { ListItem } from '@/constants/settingItem';
+import SettingItem from '@/module/setting/components/settingItem';
 
 export default function SettingPage() {
-  const { currentStyle, layoutName, themeName, changeLayout, changeTheme } =
-    useVisualScheme(
-      ({ currentStyle, layoutName, changeTheme, changeLayout, themeName }) => ({
-        currentStyle,
-        changeTheme,
-        themeName,
-        layoutName,
-        changeLayout,
-      })
-    );
-  const { isUpdateAvailable, isUpdatePending } = Updates.useUpdates();
-  const [loading, setLoading] = useState<boolean>(false);
-  useEffect(() => {
-    if (isUpdatePending) {
-      void Updates.reloadAsync();
-    }
-  }, [isUpdatePending]);
-  useEffect(() => {
-    isUpdateAvailable &&
-      Modal.show({
-        title: '检测到更新',
-        children: '是否更新',
-        onConfirm: () => {
-          Updates.fetchUpdateAsync();
-        },
-      });
-  }, [isUpdateAvailable]);
-
   return (
     <ThemeBasedView style={{ flex: 1 }}>
       <FlatList
@@ -53,46 +15,6 @@ export default function SettingPage() {
         )}
         keyExtractor={item => item.id.toString()}
       />
-      <Button
-        style={[currentStyle?.button_style, { width: '100%' }]}
-        onPress={() => {
-          changeTheme(themeName === 'dark' ? 'light' : 'dark');
-        }}
-      >
-        切换模式
-      </Button>
-      <Button
-        onPress={() => {
-          changeLayout(layoutName === 'android' ? 'ios' : 'android');
-        }}
-        style={[currentStyle?.button_style, { width: '100%' }]}
-      >
-        {'切换主题,当前主题：' + layoutName}
-      </Button>
-      <Button
-        style={[currentStyle?.button_style, { width: '100%' }]}
-        onPress={() => {
-          setLoading(true);
-          Updates.checkForUpdateAsync()
-            .then(res => {
-              if (!res.isAvailable) {
-                Toast.show({ text: '已是最新版', icon: 'success' });
-              }
-            })
-            .catch(err => {
-              Toast.show({ text: '我是谁' });
-            })
-            .finally(() => {
-              setLoading(false);
-            });
-        }}
-        isLoading={loading}
-        children="检查更新"
-      />
-      <StatusBar style="auto" />
-      <Picker>
-        <Text style={currentStyle?.text_style}>345345</Text>
-      </Picker>
     </ThemeBasedView>
   );
 }
