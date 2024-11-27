4d621fc39b6fdd7f1c24c0717f155fe97b8b42fe
eleliauk <2831336720@qq.com>
2024-11-27 21:18:46 +0800

    feat(setting): 增加settingItem的interface

---
+++ app/(setting)/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ThemeBasedView from '@/components/view';
+import { Text, View } from 'react-native';
+function About() {
+  return (
+    <ThemeBasedView>
+      <Text>关于</Text>
+    </ThemeBasedView>
+  );
+}
+export default About;
