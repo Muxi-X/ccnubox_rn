96572dba277ebbcbb063e2ae1941931d74dd297b
eleliauk <2831336720@qq.com>
2024-11-29 10:39:50 +0800

    feat(setting): 增加setting页面上边框

--- constants/settingItem.tsx
+++ constants/settingItem.tsx
@@ -1,42 +1,49 @@
-import { Href } from 'expo-router';
-interface SettingItem {
-  id: number;
-  icon: string;
-  text: string;
-  url: Href<string>;
-}
+import { SettingItem } from '@/types/settingItem';
+
 export const SettingItems: SettingItem[] = [
   {
+    title: '个性化',
+    name: '个性化',
     id: 1,
     icon: require('@/assets/images/person.png'),
     text: '个性化',
     url: '/(setting)/theme',
   },
   {
+    title: '个性化1',
+    name: '个性化1',
     id: 2,
     icon: require('@/assets/images/share.png'),
     text: '分享',
     url: '/(setting)/theme',
   },
   {
+    title: '个性化2',
+    name: '个性化2',
     id: 3,
     icon: require('@/assets/images/help.png'),
     text: '帮助与反馈',
     url: '/(setting)/theme',
   },
   {
+    title: '个性化3',
+    name: '个性化3',
     id: 4,
     icon: require('@/assets/images/check-update.png'),
     text: '检查更新',
     url: '/(setting)/theme',
   },
   {
+    title: '个性化4',
+    name: '个性化4',
     id: 5,
     icon: require('@/assets/images/about.png'),
     text: '关于',
     url: '/(setting)/about',
   },
   {
+    title: '个性化5',
+    name: '个性化5',
     id: 6,
     icon: require('@/assets/images/exit.png'),
     text: '退出',
