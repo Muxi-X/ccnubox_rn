a93636b19ea65a804454801760f288b5405048a6
eleliauk <2831336720@qq.com>
2024-11-29 10:51:55 +0800

    feat(setting): 完成setting上边框

---
+++ app/(setting)/exit.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import ThemeBasedView from '@/components/view';
+function Exit() {
+  return (
+    <ThemeBasedView>
+      <Text>关于1</Text>
+    </ThemeBasedView>
+  );
+}
+export default Exit;
