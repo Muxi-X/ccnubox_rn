52d160006a71468186ac5d13128c1d3b4ddc1c51
OldSecond <oldsecond2@qq.com>
2024-12-10 23:16:23 +0800

    feat(request): 实现 token 刷新功能
    - 添加 refreshToken 函数，用于刷新短 token
    - 在请求拦截器中集成 token 刷新逻辑
    - 优化错误处理，当 token 刷新失败时重定向到登录页面

--- request/request.ts
+++ request/request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { router } from 'expo-router';
-import { getItem } from 'expo-secure-store';
+import { getItem, setItem } from 'expo-secure-store';
 
 import Toast from '@/components/toast';
 
@@ -15,12 +15,42 @@ const axiosInstance: AxiosInstance = axios.create({
 
 async function getStoredToken(): Promise<string> {
   try {
-    const token = getItem('shortToken');
-    if (token) return token;
+    const shortToken = getItem('shortToken');
+    if (shortToken) return shortToken;
+
+    // 如果短 token 不存在，尝试刷新
+    return await refreshToken();
   } catch (error) {
     console.error('获取 token 失败:', error);
+    throw new Error('token不存在');
   }
-  throw new Error('token不存在');
+}
+
+async function refreshToken(): Promise<string> {
+  try {
+    const longToken = getItem('longToken');
+    if (!longToken) throw new Error('长 token 不存在');
+
+    // 刷新短 token
+    const response = await axios.get(
+      `${process.env.EXPO_PUBLIC_API_URL}/users/refresh_token`,
+      {
+        headers: { Authorization: `Bearer ${longToken}` },
+      }
+    );
+
+    if (response.status === 200 || response.status === 201) {
+      const newShortToken = response.headers['x-jwt-token'];
+      console.log(newShortToken);
+      setItem('shortToken', newShortToken);
+      return newShortToken;
+    }
+
+    throw new Error('刷新短 token 失败');
+  } catch (error) {
+    console.error('刷新短 token 失败:', error);
+    throw error;
+  }
 }
 
 axiosInstance.interceptors.request.use(
@@ -51,7 +81,6 @@ axiosInstance.interceptors.response.use(
         return response;
       case 401:
         console.error('token过期');
-        router.replace('/auth/login');
         break;
       case 403:
         Toast.show({ text: '无权限' });
@@ -66,9 +95,23 @@ axiosInstance.interceptors.response.use(
       return Promise.reject(new Error(`Error status code: ${response.status}`));
     }
   },
-  error => {
-    console.error('Error response:', error);
+  async error => {
     requestBus.requestComplete();
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // 防止无限循环
+      try {
+        const newShortToken = await refreshToken();
+        originalRequest.headers['Authorization'] = `Bearer ${newShortToken}`;
+        return axiosInstance(originalRequest); // 重新发送请求
+      } catch (refreshError) {
+        console.error('刷新 token 失败:', refreshError);
+        router.replace('/auth/login');
+      }
+    }
+
+    console.error('Error response:', error);
     return Promise.reject(error);
   }
 );
