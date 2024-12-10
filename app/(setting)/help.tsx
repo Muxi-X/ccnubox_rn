a71bf6bdaa54df0ed63a83879cdcfbc611e7df29
eleliauk <2831336720@qq.com>
2024-12-10 10:56:23 +0800

    feat(help): 新增帮助页面

--- app/(setting)/help.tsx
+++ app/(setting)/help.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { SearchBar } from '@ant-design/react-native';
+import React, { useState } from 'react';
+import { Image, Text, View } from 'react-native';
 
 import ThemeBasedView from '@/components/view';
+
 function Help() {
+  const [value, setValue] = useState('');
   return (
     <ThemeBasedView>
-      <Text>关于</Text>
+      <SearchBar
+        placeholder="请输入问题"
+        value={value}
+        onChange={value => setValue(value)}
+        onSubmit={value => {
+          console.log(value);
+          setValue('');
+        }}
+      />
+      <View>
+        <Image source={require('@/assets/images/check-update.png')} />
+        <Text>常见问题</Text>
+      </View>
     </ThemeBasedView>
   );
 }
