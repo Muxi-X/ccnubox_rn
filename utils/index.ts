36bd004b0778a45b8c5caefbc8a70f0d4dbc8143
墨綠BG <s5460703@gmail.com>
2024-12-19 02:28:22 +0800

    :hammer: refactor(utils)：安全化 percent2px 参数类型

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,4 @@ export { default as EventBus } from './eventBus';
 export { default as fetchUpdate } from './fetchUpdate';
 export { default as generateStyleSheet } from './generateStyleSheet';
 export { default as keyGenerator } from './keyGenerator';
+export { default as percent2px } from './percent2px';
