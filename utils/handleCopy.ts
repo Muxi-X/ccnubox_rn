8f64677bc718c0ad654569938d8c22604b6645dc
eleliauk <2831336720@qq.com>
2025-02-18 14:14:31 +0800

    feat(website): 添加网站列表组件，集成API请求并实现复制功能；优化样式

---
+++ utils/handleCopy.ts
@@ -0,0 +1,19 @@
+import * as Clipboard from 'expo-clipboard';
+
+import Modal from '@/components/modal';
+/** 用户复制文字
+ * 成功后 Modal展示复制成功
+ * @param text 要复制的文字
+ */
+const handleCopy = (text: string) => {
+  Clipboard.setStringAsync(text).then(() => {
+    Modal.show({
+      title: '复制成功',
+      children: '文本已复制到剪贴板',
+      mode: 'middle',
+      showCancel: false,
+    });
+  });
+  console.log('复制文本为：' + text);
+};
+export default handleCopy;
