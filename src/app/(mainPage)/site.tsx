1ce9e904b356221c77bd063094bec53450de575f
梦想跟老登一样厉害 <148364842+eleliauk@users.noreply.github.com>
2025-03-25 08:17:52 +0800

    feat(pages): 图书馆预约和校历页面

--- src/app/(mainPage)/site.tsx
+++ src/app/(mainPage)/site.tsx
@@ -1,5 +1,19 @@
-import { Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function Site() {
-  return <Text>SpaceLesson</Text>;
+  return (
+    <WebView
+      style={styles.container}
+      source={{
+        uri: 'https://account.ccnu.edu.cn/cas/login?service=http://kjyy.ccnu.edu.cn/loginall.aspx?page=&pageId=1053906&wfwfid=1740&websiteId=548973',
+      }}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
