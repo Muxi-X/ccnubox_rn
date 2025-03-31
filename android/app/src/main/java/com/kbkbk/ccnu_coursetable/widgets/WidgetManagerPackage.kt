7aa83094e2d208927579e2399f9876f575feec00
EdmundL1z <edmund.1zqqq@gmail.com>
2025-03-31 16:31:52 +0800

    feat(components): 数据通信测试（native）

---
+++ android/app/src/main/java/com/kbkbk/ccnu_coursetable/widgets/WidgetManagerPackage.kt
@@ -0,0 +1,17 @@
+package com.kbkbk.ccnu_coursetable.widgets
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class WidgetManagerPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf<NativeModule>(WidgetManagerModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
