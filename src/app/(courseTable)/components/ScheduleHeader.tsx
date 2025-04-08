4dd0cd8a2da78f1ea7cb57aa88bc4dbf6f01085e
bingling-sama <bingling_sama@qq.com>
2025-04-08 22:22:49 +0800

    feat(components): integrate visual scheme in ScheduleHeader

---
+++ src/app/(courseTable)/components/ScheduleHeader.tsx
@@ -0,0 +1,133 @@
+import { Tooltip } from '@ant-design/react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Href, router } from 'expo-router';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import useVisualScheme from '@/store/visualScheme';
+import useWeekStore from '@/store/weekStore';
+
+import { tooltipActions } from '@/constants/courseTableApplications';
+import { commonStyles } from '@/styles/common';
+import globalEventBus from '@/utils/eventBus';
+
+export const ScheduleHeaderTitle: React.FC = () => {
+  // const [showWeekPicker, setShowWeekPicker] = React.useState(false);
+  const { currentWeek, showWeekPicker, setShowWeekPicker } = useWeekStore();
+
+  return (
+    <>
+      <View
+        style={{
+          width: '60%',
+          margin: 'auto',
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+          }}
+          onPress={() => {
+            // console.log('选择周次');
+            // setShowWeekPicker(!showWeekPicker);
+            setShowWeekPicker(!showWeekPicker);
+          }}
+        >
+          <Text
+            style={[
+              commonStyles.fontLarge,
+              useVisualScheme.getState().currentStyle?.header_text_style,
+              {
+                textAlign: 'center',
+              },
+            ]}
+          >
+            第{currentWeek}周
+          </Text>
+          <MaterialIcons
+            name="arrow-forward-ios"
+            size={20}
+            style={[
+              useVisualScheme.getState().currentStyle?.header_text_style,
+              {
+                transform: [{ rotate: '90deg' }],
+                marginLeft: 4,
+              },
+            ]}
+          />
+        </TouchableOpacity>
+        <Text
+          style={[
+            commonStyles.fontLight,
+            commonStyles.fontSmall,
+            useVisualScheme.getState().currentStyle?.schedule_week_text_style,
+            {
+              textAlign: 'center',
+            },
+          ]}
+        >
+          当前周设置为{currentWeek}
+        </Text>
+      </View>
+    </>
+  );
+};
+
+export const ScheduleHeaderRight: React.FC = () => {
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
+      <MaterialIcons
+        name="delete-sweep"
+        size={24}
+        style={[
+          useVisualScheme.getState().currentStyle?.header_text_style,
+          {
+            paddingRight: 10,
+          },
+        ]}
+      />
+      <View>
+        <Tooltip.Menu
+          actions={tooltipActions}
+          placement="bottom-start"
+          onAction={node => {
+            if (node.key === 'screenShot') {
+              globalEventBus.emit('SaveImageShot');
+            }
+            if ((node.key as string)[0] === '/') {
+              router.navigate(node.key as Href);
+            }
+          }}
+          styles={{
+            tooltip: {
+              width: 160,
+            },
+          }}
+          trigger="onPress"
+        >
+          <TouchableOpacity>
+            <MaterialIcons
+              name="add"
+              size={24}
+              style={[
+                useVisualScheme.getState().currentStyle?.header_text_style,
+                {
+                  paddingRight: 10,
+                },
+              ]}
+            />
+          </TouchableOpacity>
+        </Tooltip.Menu>
+      </View>
+    </View>
+  );
+};
--- src/constants/tabBar.tsx
+++ src/constants/tabBar.tsx
@@ -1,85 +1,17 @@
-import { Tooltip } from '@ant-design/react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { Href, router } from 'expo-router';
 import * as React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text } from 'react-native';
 
 import useVisualScheme from '@/store/visualScheme';
-import useWeekStore from '@/store/weekStore';
 
+import {
+  ScheduleHeaderRight,
+  ScheduleHeaderTitle,
+} from '@/app/(courseTable)/components/ScheduleHeader';
 import NotificationHeaderRight from '@/modules/notification/component/NotiNavbar';
 import { commonColors, commonStyles } from '@/styles/common';
-import globalEventBus from '@/utils/eventBus';
-
-import { tooltipActions } from './courseTableApplications';
 
 import { SinglePageType } from '@/types/tabBarTypes';
-
-const ScheduleHeader: React.FC = () => {
-  // const [showWeekPicker, setShowWeekPicker] = React.useState(false);
-  const { currentWeek, showWeekPicker, setShowWeekPicker } = useWeekStore();
-
-  return (
-    <>
-      <View
-        style={{
-          width: '60%',
-          margin: 'auto',
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-          }}
-          onPress={() => {
-            // console.log('选择周次');
-            // setShowWeekPicker(!showWeekPicker);
-            setShowWeekPicker(!showWeekPicker);
-          }}
-        >
-          <Text
-            style={[
-              commonStyles.fontLarge,
-              useVisualScheme.getState().currentStyle?.header_text_style,
-              {
-                textAlign: 'center',
-              },
-            ]}
-          >
-            第{currentWeek}周
-          </Text>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={20}
-            style={[
-              useVisualScheme.getState().currentStyle?.header_text_style,
-              {
-                transform: [{ rotate: '90deg' }],
-                marginLeft: 4,
-              },
-            ]}
-          />
-        </TouchableOpacity>
-        <Text
-          style={[
-            commonStyles.fontLight,
-            commonStyles.fontSmall,
-            useVisualScheme.getState().currentStyle?.schedule_week_text_style,
-            {
-              textAlign: 'center',
-            },
-          ]}
-        >
-          当前周设置为{currentWeek}
-        </Text>
-      </View>
-    </>
-  );
-};
 
 /**
  * @enum tabBar颜色
@@ -122,60 +54,8 @@ export const tabConfig: SinglePageType[] = [
     name: 'schedule',
     title: '日程',
     iconName: 'calendar',
-    headerTitle: () => <ScheduleHeader />,
-    headerRight: () => (
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
-      >
-        <MaterialIcons
-          name="delete-sweep"
-          size={24}
-          style={[
-            useVisualScheme.getState().currentStyle?.header_text_style,
-            {
-              paddingRight: 10,
-            },
-          ]}
-        />
-        <View>
-          <Tooltip.Menu
-            actions={tooltipActions}
-            placement="bottom-start"
-            onAction={node => {
-              if (node.key === 'screenShot') {
-                globalEventBus.emit('SaveImageShot');
-              }
-              if ((node.key as string)[0] === '/') {
-                router.navigate(node.key as Href);
-              }
-            }}
-            styles={{
-              tooltip: {
-                width: 160,
-              },
-            }}
-            trigger="onPress"
-          >
-            <TouchableOpacity>
-              <MaterialIcons
-                name="add"
-                size={24}
-                style={[
-                  useVisualScheme.getState().currentStyle?.header_text_style,
-                  {
-                    paddingRight: 10,
-                  },
-                ]}
-              />
-            </TouchableOpacity>
-          </Tooltip.Menu>
-        </View>
-      </View>
-    ),
+    headerTitle: () => <ScheduleHeaderTitle />,
+    headerRight: () => <ScheduleHeaderRight />,
   },
   {
     name: 'notification',
