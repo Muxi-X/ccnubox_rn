6963baa99a85122176a12914f84cb7330bdceb34
bingling-sama <bingling_sama@qq.com>
2025-04-29 14:33:58 +0800

    refactor(pages): course table refresh logic

--- src/store/time.ts
+++ src/store/time.ts
@@ -5,10 +5,6 @@ interface TimeState {
   setCurrentWeek: (_week: number) => void;
   showWeekPicker: boolean;
   setShowWeekPicker: (_opened: boolean) => void;
-  holidayTime: number;
-  setHolidayTime: (_time: number) => void;
-  schoolTime: number;
-  setSchoolTime: (_time: number) => void;
 }
 
 const useTimeStore = create<TimeState>(set => ({
@@ -17,10 +13,6 @@ const useTimeStore = create<TimeState>(set => ({
   showWeekPicker: false,
   setShowWeekPicker: (showWeekPicker: boolean) =>
     set({ showWeekPicker: showWeekPicker }),
-  holidayTime: 0,
-  setHolidayTime: (_time: number) => set({ holidayTime: _time }),
-  schoolTime: 0,
-  setSchoolTime: (_time: number) => set({ schoolTime: _time }),
 }));
 
 export default useTimeStore;
