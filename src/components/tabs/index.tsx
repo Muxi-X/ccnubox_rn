702d16669ea2dc44615518c63599151017113aa1
bingling-sama <bingling_sama@qq.com>
2025-05-10 16:21:41 +0800

    fix(pages): fix tabbar component dark mode style

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,7 +1,10 @@
 import { Tabs, TabsProps } from '@ant-design/react-native';
 import React from 'react';
 
+import useVisualScheme from '@/store/visualScheme';
+
 export const TabBar: React.FC<TabsProps> = props => {
+  const currentStyle = useVisualScheme(state => state.currentStyle);
   return (
     <Tabs
       {...props}
@@ -12,12 +15,16 @@ export const TabBar: React.FC<TabsProps> = props => {
             fontSize: 18,
             fontWeight: 500,
           }}
+          tabBarInactiveTextColor={currentStyle?.text_style?.color as string}
           tabBarActiveTextColor="#9379F6"
           tabBarUnderlineStyle={{
             backgroundColor: '#9379F6',
             marginHorizontal: '10%',
             width: '30%',
           }}
+          tabBarBackgroundColor={
+            currentStyle?.background_style?.backgroundColor as string
+          }
           styles={{
             tab: {
               height: 60,
