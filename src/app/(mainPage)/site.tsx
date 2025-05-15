38aae66c2f3e9716c4fde8db5b334a35acff055b
bingling-sama <bingling_sama@qq.com>
2025-05-15 17:25:45 +0800

    refactor(components): improve dark mode components

---
+++ src/app/(mainPage)/site.tsx
@@ -0,0 +1,10 @@
+import Text from '@/components/text';
+import View from '@/components/view';
+
+export default function Site() {
+  return (
+    <View>
+      <Text>座位预约</Text>
+    </View>
+  );
+}
