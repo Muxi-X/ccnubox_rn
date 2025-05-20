33178854739127796c4bd6a5d8948bf0f2fba681
bingling-sama <bingling_sama@qq.com>
2025-05-20 14:47:21 +0800

    chore(config): disable some eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,103 +11,104 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: eslintPluginJs.configs.recommended,
-  allConfig: eslintPluginJs.configs.all,
+	baseDirectory: __dirname,
+	recommendedConfig: eslintPluginJs.configs.recommended,
+	allConfig: eslintPluginJs.configs.all,
 });
 
 const eslintConfig = [
-  ...compat.config({
-    env: {
-      browser: true,
-      es2024: true,
-      node: true,
-    },
-  }),
-  {
-    ignores: [
-      '**/node_modules/**',
-      '**/expo/**',
-      '**/.vscode/**',
-      '**/.husky/**',
-      '**/.idea/**',
-      'metro.config.js',
-      'expo-env.d.ts',
-      '**/scripts/**',
-      'plugin.js',
-    ],
-    languageOptions: {
-      globals: {
-        React: 'readonly',
-        JSX: 'readonly',
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      'simple-import-sort': eslintPluginSimpleImportSort,
-      'unused-imports': unusedImports,
-      'import': eslintPluginImport,
-    },
-    rules: {
-      'no-duplicate-imports': 'error',
-      'import/first': 'error',
-      'import/no-duplicates': 'error',
-      'import/no-useless-path-segments': 'error',
-      'react-hooks/exhaustive-deps': 'off',
-      'no-unused-vars': 'off',
-      'no-console': 'warn',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'react/no-unescaped-entities': 'off',
-      'react/display-name': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-      'simple-import-sort/exports': 'warn',
-      'simple-import-sort/imports': [
-        'warn',
-        {
-          groups: [
-            ['^@?\\w', '^\\u0000'],
-            ['^.+\\.s?css$'],
-            ['^@/libs', '^@/hooks'],
-            ['^@/data'],
-            ['^@/components', '^@/container'],
-            ['^@/store'],
-            ['^@/'],
-            [
-              '^\\./?$',
-              '^\\.(?!/?$)',
-              '^\\.\\./?$',
-              '^\\.\\.(?!/?$)',
-              '^\\.\\./\\.\\./?$',
-              '^\\.\\./\\.\\.(?!/?$)',
-              '^\\.\\./\\.\\./\\.\\./?$',
-              '^\\.\\./\\.\\./\\.\\.(?!/?$)',
-            ],
-            ['^@/types'],
-            ['^'],
-          ],
-        },
-      ],
-    },
-  },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ),
+	...compat.extends(
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	),
+	...compat.config({
+		env: {
+			browser: true,
+			es2024: true,
+			node: true,
+		},
+	}),
+	{
+		ignores: [
+			'**/node_modules/**',
+			'**/expo/**',
+			'**/.vscode/**',
+			'**/.husky/**',
+			'**/.idea/**',
+			'metro.config.js',
+			'expo-env.d.ts',
+			'**/scripts/**',
+			'plugin.js',
+		],
+		languageOptions: {
+			globals: {
+				React: 'readonly',
+				JSX: 'readonly',
+			},
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+			'simple-import-sort': eslintPluginSimpleImportSort,
+			'unused-imports': unusedImports,
+			'import': eslintPluginImport,
+		},
+		rules: {
+			'no-duplicate-imports': 'error',
+			'import/first': 'error',
+			'import/no-duplicates': 'error',
+			'import/no-useless-path-segments': 'error',
+			'react-hooks/exhaustive-deps': 'off',
+			'no-unused-vars': 'off',
+			'no-console': 'warn',
+			'react/no-unescaped-entities': 'off',
+			'react/display-name': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'unused-imports/no-unused-imports': 'warn',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+			'simple-import-sort/exports': 'warn',
+			'simple-import-sort/imports': [
+				'warn',
+				{
+					groups: [
+						['^@?\\w', '^\\u0000'],
+						['^.+\\.s?css$'],
+						['^@/libs', '^@/hooks'],
+						['^@/data'],
+						['^@/components', '^@/container'],
+						['^@/store'],
+						['^@/'],
+						[
+							'^\\./?$',
+							'^\\.(?!/?$)',
+							'^\\.\\./?$',
+							'^\\.\\.(?!/?$)',
+							'^\\.\\./\\.\\./?$',
+							'^\\.\\./\\.\\.(?!/?$)',
+							'^\\.\\./\\.\\./\\.\\./?$',
+							'^\\.\\./\\.\\./\\.\\.(?!/?$)',
+						],
+						['^@/types'],
+						['^'],
+					],
+				},
+			],
+		},
+	},
 ];
 
 export default eslintConfig;
