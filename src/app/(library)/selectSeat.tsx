d43296c940550db0c84bb6f5fd2c89837035e1fb
lwp <1096152629@qq.com>
2025-09-20 14:48:11 +0800

    refactor(components): 修改普通选座的功能逻辑

--- src/app/(library)/selectSeat.tsx
+++ src/app/(library)/selectSeat.tsx
@@ -5,17 +5,20 @@ import TabBar from '@/components/tabs';
 import FastSelect from '@/modules/library/seats/fastSelect';
 import ManualSelect from '@/modules/library/seats/manualSelect';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function ViewSeats() {
   return (
-    <View style={{ flex: 1 }}>
-      <TabBar tabs={[{ title: '普通选座' }, { title: '快速选座' }]}>
-        <View style={{ flex: 1 }}>
-          <ManualSelect />
-        </View>
-        <View style={{ flex: 1 }}>
-          <FastSelect />
-        </View>
-      </TabBar>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <TabBar tabs={[{ title: '普通选座' }, { title: '快速选座' }]}>
+          <View style={{ flex: 1 }}>
+            <ManualSelect />
+          </View>
+          <View style={{ flex: 1 }}>
+            <FastSelect />
+          </View>
+        </TabBar>
+      </View>
+    </GestureHandlerRootView>
   );
 }
