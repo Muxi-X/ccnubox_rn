1e7c64eaf40cbf7dac84ce63c1f4e9124b7dbb79
bingling-sama <bingling_sama@qq.com>
2025-12-02 23:11:10 +0800

    feat(pages): main page icon sync with theme

--- src/types/mainPageGridTypes.ts
+++ src/types/mainPageGridTypes.ts
@@ -4,10 +4,8 @@ import { SvgProps } from 'react-native-svg';
 
 import { SinglePageType } from '@/types/tabBarTypes';
 
-export interface MainPageGridDataType extends Omit<
-  SinglePageType,
-  'iconName' | 'headerLeft'
-> {
+export interface MainPageGridDataType
+  extends Omit<SinglePageType, 'iconName' | 'headerLeft'> {
   imageUrl: ImageSourcePropType | React.FC<SvgProps>;
   href?: Href;
   action?: () => void;
