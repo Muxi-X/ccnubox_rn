e10f3f057634be0233e0b5157c4d9db7c2c36206
超级无敌数码暴龙战士 <1845519693@qq.com>
2025-12-26 11:45:23 +0800

    chore(utils): remove dotenv-flow and update JPush configuration

--- src/secret/JPush.ts
+++ src/secret/JPush.ts
@@ -3,6 +3,6 @@
  * 从环境变量中读取
  */
 export const JPushSecrets = {
-  appKey: process.env.EXPO_PUBLIC_JPUSH_APP_KEY || '',
-  channel: process.env.EXPO_PUBLIC_JPUSH_CHANNEL || '',
+  appKey: process.env.JPUSH_APP_KEY || '',
+  channel: process.env.JPUSH_CHANNEL || '',
 };
