55a7a36929fde19f8c6a215f0057d01bc66b817d
cypher <2820668937@qq.com>
2025-12-29 16:26:32 +0800

    refactor(components): add dynamic course category loading

--- src/modules/mainPage/components/checkgrades/index.tsx
+++ src/modules/mainPage/components/checkgrades/index.tsx
@@ -1,24 +1,62 @@
+import { Toast } from '@ant-design/react-native';
 import { router } from 'expo-router';
-import React, { useState } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import MultiPicker from '@/components/picker/multiPicker';
+import { PickerDataType } from '@/components/picker/types';
 
+import useCourse from '@/store/course';
 import useVisualScheme from '@/store/visualScheme';
 
+import { queryGradeType } from '@/request/api/grade';
 import { generateSemesterOptions } from '@/utils/generateSemesterOptions';
 
-import { COURSE_TYPE_OPTIONS } from './constants';
+const formatCourseType = (types: string[]) => {
+  return types.map(type => ({ label: type, value: type }));
+};
 
 const CheckGrades = () => {
+  const { courseCategories, updatecourseCategories } = useCourse();
   const semesterOptions = generateSemesterOptions();
+  const [loading, setLoading] = useState(false);
   const currentStyle = useVisualScheme(state => state.currentStyle);
-  const [selectedCourseType, setSelectedCourseType] = useState<
-    (number | string)[]
-  >(COURSE_TYPE_OPTIONS.map(option => option.value));
+  const [courseType, setCourseType] = useState<PickerDataType>([
+    formatCourseType(courseCategories),
+  ]);
+  const [selectedCourseType, setSelectedCourseType] =
+    useState<(number | string)[]>(courseCategories);
   const [selectedSemester, setSelectedSemester] = useState<(number | string)[]>(
     semesterOptions.flatMap(option => option.map(item => item.value))
   );
+
+  useEffect(() => {
+    setLoading(true);
+    queryGradeType()
+      .then(res => {
+        // tips: kcxz是课程性质
+        if (res.code === 0 && res.data?.kcxzmc) {
+          const kcxz = res.data.kcxzmc;
+
+          setSelectedCourseType(kcxz);
+          setCourseType([formatCourseType(kcxz)]);
+          // 缓存课程类别
+          updatecourseCategories(kcxz);
+        }
+      })
+      .catch(() => {
+        Toast.fail('获取课程性质失败');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   const SemesterPickerTrigger = (
     <View style={[styles.item, styles.itemBorder]}>
       <Image
@@ -78,58 +116,69 @@ const CheckGrades = () => {
 
   return (
     <View>
-      <MultiPicker
-        mode="bottom"
-        data={semesterOptions}
-        defaultValue={selectedSemester}
-        onConfirm={val => setSelectedSemester(val)}
-        onClose={() => {}}
-        showCancel={true}
-        confirmText="确认"
-        cancelText="取消"
-        titleDisplayLogic={(_val, _data) => '请选择学期'}
-      >
-        {SemesterPickerTrigger}
-      </MultiPicker>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#7878F8" />
+          <Text style={[styles.loadingText, currentStyle?.text_style]}>
+            加载中...
+          </Text>
+        </View>
+      ) : (
+        <>
+          <MultiPicker
+            mode="bottom"
+            data={semesterOptions}
+            defaultValue={selectedSemester}
+            onConfirm={val => setSelectedSemester(val)}
+            onClose={() => {}}
+            showCancel={true}
+            confirmText="确认"
+            cancelText="取消"
+            titleDisplayLogic={(_val, _data) => '请选择学期'}
+          >
+            {SemesterPickerTrigger}
+          </MultiPicker>
 
-      <MultiPicker
-        mode="bottom"
-        data={[COURSE_TYPE_OPTIONS]}
-        defaultValue={selectedCourseType}
-        onConfirm={val => setSelectedCourseType(val)}
-        onClose={() => {}}
-        showCancel={true}
-        confirmText="确认"
-        cancelText="取消"
-        titleDisplayLogic={(_val, _data) => '请选择课程性质'}
-      >
-        {CourseTypePickerTrigger}
-      </MultiPicker>
+          <MultiPicker
+            mode="bottom"
+            data={courseType}
+            defaultValue={selectedCourseType}
+            onConfirm={val => setSelectedCourseType(val)}
+            onClose={() => {}}
+            showCancel={true}
+            confirmText="确认"
+            cancelText="取消"
+            titleDisplayLogic={(_val, _data) => '请选择课程性质'}
+          >
+            {CourseTypePickerTrigger}
+          </MultiPicker>
 
-      <TouchableOpacity
-        style={{
-          width: 309,
-          height: 46,
-          backgroundColor: '#7878F8',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          margin: 'auto',
-          borderRadius: 10,
-          marginTop: 26,
-        }}
-        onPress={() => {
-          router.push({
-            pathname: '/(mainPage)/scoreCalculation',
-            params: {
-              semester: JSON.stringify(selectedSemester),
-              courseType: JSON.stringify(selectedCourseType),
-            },
-          });
-        }}
-      >
-        <Text style={{ color: '#FFFFFF' }}>查询</Text>
-      </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: 309,
+              height: 46,
+              backgroundColor: '#7878F8',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              margin: 'auto',
+              borderRadius: 10,
+              marginTop: 26,
+            }}
+            onPress={() => {
+              router.push({
+                pathname: '/(mainPage)/scoreCalculation',
+                params: {
+                  semester: JSON.stringify(selectedSemester),
+                  courseType: JSON.stringify(selectedCourseType),
+                },
+              });
+            }}
+          >
+            <Text style={{ color: '#FFFFFF' }}>查询</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 };
@@ -149,5 +198,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.5,
     borderStyle: 'solid',
     borderColor: '#D8D8D880',
+  },
+  loadingContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  loadingText: {
+    marginTop: 10,
+    color: '#666',
   },
 });
